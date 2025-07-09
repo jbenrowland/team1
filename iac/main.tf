a6cc0dd8ad6425192911d4aed3cb5c6266665da0
Ben <jbenrowland14@gmail.com>
2025-07-09 19:15:35 +0000

    Updated Terraform to deploy jbenrowland/team1 Docker image

--- iac/main.tf
+++ iac/main.tf
@@ -1,4 +1,3 @@
-#AWS provider using us-west-2 (Oregon)
 provider "aws" {
   region = "us-west-2"
 }
@@ -7,7 +6,6 @@ resource "random_pet" "suffix" {
   length = 2
 }
 
-#Fetch latest Amazon Linux 2 AMI
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
@@ -18,7 +16,6 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-#Use default VPC and subnet
 data "aws_vpc" "default" {
   default = true
 }
@@ -30,20 +27,19 @@ data "aws_subnets" "default" {
   }
 }
 
-#Security group that allows only HTTP traffic (no SSH)
 resource "aws_security_group" "web_sg" {
   name        = "web-sg-${random_pet.suffix.id}"
   description = "Allow HTTP traffic only"
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
-    description = "Allow HTTP from anywhere"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress { #allow ssh
+
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -58,7 +54,6 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
-#EC2 instance that builds and runs your Docker app
 resource "aws_instance" "app" {
   ami                         = data.aws_ami.amazon_linux.id
   instance_type               = "t2.micro"
@@ -66,38 +61,21 @@ resource "aws_instance" "app" {
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
 
-  # User data script to deploy the app from GitHub
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
               amazon-linux-extras install docker -y
               service docker start
               usermod -a -G docker ec2-user
-              yum install git -y
-              cd /home/ec2-user
-
-              # grab the docker image from dockerhub. We need to make sure we update the user name
-              docker pull squirest/bulletin-board-app:latest
-              docker run -d -p 80:8080 squirest/bulletin-board-app:latest
-
-              # #####################################################################
-              # If you wanted to clone your GitHub repo that contains the Dockerfile 
-              # #####################################################################
-              # git clone https://github.com/byui-itm350-s25/trevor-squires-personal.git
-              # Navigate to the app directory inside the repo
-              # cd trevor-squires-personal/bulletin-board-app
-              # Build and run the Docker container
-              # docker build -t bulletin-board .
-              # docker run -d -p 80:8080 bulletin-board
-              # #####################################################################
-
-
+              docker pull jbenrowland/team1:latest
+              docker run -d -p 80:3000 jbenrowland/team1:latest
               EOF
+
   tags = {
-    Name = "bulletin-board-app"
+    Name = "biodrop-app"
   }
 }
-#Output the public IP address so you can access the app
+
 output "instance_public_ip" {
   description = "Visit http://<this-ip> to access the app"
   value       = aws_instance.app.public_ip
