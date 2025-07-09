d9c5f669bc995185743d3dbbb10d1229ad38916d
Ben <jbenrowland14@gmail.com>
2025-07-09 22:10:24 +0000

    test

--- iac/main.tf
+++ iac/main.tf
@@ -2,84 +2,31 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "random_pet" "suffix" {
-  length = 2
-}
-
 data "aws_ami" "amazon_linux" {
   most_recent = true
-  owners      = ["amazon"]
 
   filter {
     name   = "name"
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
+
+  owners = ["amazon"]
+}
+
+resource "random_pet" "suffix" {
+  length    = 2
+  separator = "-"
 }
 
 data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-resource "aws_security_group" "web_sg" {
-  name        = "web-sg-${random_pet.suffix.id}"
-  description = "Allow HTTP traffic only"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    description = "Allow HTTP from anywhere"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "app" {
-  ami                         = data.aws_ami.amazon_linux.id
-  instance_type               = "t2.micro"
-  subnet_id                   = data.aws_subnets.default.ids[0]
-  vpc_security_group_ids      = [aws_security_group.web_sg.id]
-  associate_public_ip_address = true
-
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              amazon-linux-extras install docker -y
-              service docker start
-              usermod -a -G docker ec2-user
-              yum install git -y
-              cd /home/ec2-user
-              docker pull jbenrowland/team1:latest
-              docker run -d -p 80:8080 jbenrowland/team1:latest
-              EOF
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "team1-app"
+    Name = "Team1-${random_pet.suffix.id}"
   }
-}
-
-output "instance_public_ip" {
-  description = "Public IP of the instance"
-  value       = aws_instance.app.public_ip
 }
